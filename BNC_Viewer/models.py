2f7b638a0b8924955658cb215e8c75a2d9198cf7
RohitMetaCube <rohit.kumar@metacube.com>
2016-02-17 17:52:42 +0530

    Revert ":tada: Added .gitattributes & .gitignore files"
    This reverts commit 921be95a4cf6234099978ddbe8839687e90450ed.

---
+++ BNC_Viewer/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+"""class Person(models.Model):
+    ActorID =  models.PositiveIntegerField()
+    GivenName1 = models.CharField(max_length=255)
+    GivenName2 = models.CharField(max_length=255)
+    FamilyName1 = models.CharField(max_length=255)
+    FamilyName2 = models.CharField(max_length=255)
+    LocationID =  models.PositiveIntegerField()
+    Occupation = models.CharField(max_length=45)
+    NationalID = models.CharField(max_length=45)
+    NationalIDType = models.CharField(max_length=45)
+    Nationality = models.CharField(max_length=2)"""
